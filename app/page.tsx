fa96c7bedac81c9221f372291f1d6b4be5ca0836
Mathilde Schmid <mathilde.schmid@armtek.fr>
2025-08-25 13:40:56 +0200

    Projet fini

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,124 @@
-import Image from "next/image";
+"use client";
+import { Layers } from "lucide-react";
+import Wrapper from "./components/Wrapper";
+import { useEffect, useState } from "react";
+import { createEmptyInvoice, getInvoicesByEmail } from "./actions";
+import { useUser } from "@clerk/nextjs";
+import confetti from "canvas-confetti";
+import { Invoice } from "@/type";
+import InvoiceComponent from "./components/InvoiceComponent";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const { user } = useUser();
+  const [invoiceName, setInvoiceName] = useState("");
+  const [isNameValid, setIsNameValid] = useState(true);
+  const email = user?.primaryEmailAddress?.emailAddress as string;
+  const [invoices, setInvoices] = useState<Invoice[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  //recupérer la liste des factures
+  const fetchInvoices = async () => {
+    try {
+      const data = await getInvoicesByEmail(email);
+      if (data) {
+        setInvoices(data);
+      }
+    } catch (error) {
+      console.error("Erreur lors du chargement des factures", error);
+    }
+  };
+
+  useEffect(() => {
+    setIsNameValid(invoiceName.length <= 60);
+  }, [invoiceName]);
+
+  useEffect(() => {
+    fetchInvoices();
+  }, [email]);
+
+  const handleCreateInvoice = async () => {
+    try {
+      if (email) {
+        await createEmptyInvoice(email, invoiceName);
+      }
+      fetchInvoices();
+      setInvoiceName("");
+      const modal = document.getElementById("my_modal_3") as HTMLDialogElement;
+      if (modal) {
+        modal.close();
+      }
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 },
+        zIndex: 9999,
+        // colors: ["#ff0000", "#00ff00", "#0000ff"],
+      });
+    } catch (error) {
+      console.error("Erreur lors de la création de la fature :", error);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <div className="felx flex-col space-y-4">
+        <h1 className="text-lg font-bold">Mes factures</h1>
+
+        <div className="grid md:grid-cols-3 gap-4">
+          <div
+            className="cursor-pointer border border-accent rounded-xl flex flex-col justify-center items-center p-5"
+            onClick={() =>
+              (
+                document.getElementById("my_modal_3") as HTMLDialogElement
+              ).showModal()
+            }
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="font-bold text-accent">Créer une facture</div>
+            <div className="bg-accent-content text-accent rounded-full p-2 mt-2">
+              <Layers className="h-6 w-6" />
+            </div>
+          </div>
+
+          {/* Liste des factures */}
+          {invoices.length > 0 &&
+            invoices.map((invoice, index) => (
+              <div key={index}>
+                <InvoiceComponent invoice={invoice} index={index} />
+              </div>
+            ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+            <h3 className="font-bold text-lg">Nouvelle Facture</h3>
+            <input
+              type="text"
+              placeholder="Nom de la facture (max 60 caractères)"
+              className="input input-bordered w-full my-4"
+              value={invoiceName}
+              onChange={(e) => setInvoiceName(e.target.value)}
+            />
+
+            {!isNameValid && (
+              <p className="mb-4 txt-sm">
+                Le nom ne peut pas dépasser 60 caractères
+              </p>
+            )}
+
+            <button
+              className="btn btn-accent"
+              disabled={!isNameValid || invoiceName.length === 0}
+              onClick={handleCreateInvoice}
+            >
+              Créer
+            </button>
+          </div>
+        </dialog>
+      </div>
+    </Wrapper>
   );
 }
